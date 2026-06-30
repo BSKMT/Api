f8e30574df61c704f4d4f241014f9076cd566f3d
James Cespedes <cespedesandres684@gmail.com>
2026-06-30 08:03:58 -0500

    fix: import UsersModule in NotificationsModule for SessionGuard dependency

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -6,12 +6,14 @@ import {
 } from "./schemas/notification.schema";
 import { NotificationsService } from "./notifications.service";
 import { NotificationsController } from "./notifications.controller";
+import { UsersModule } from "../users/users.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Notification.name, schema: NotificationSchema },
     ]),
+    UsersModule,
   ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
